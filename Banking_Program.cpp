bdc8b8d27565e72653382c953614712a81146de9
Diorduc Sorin <123559538+Diorduc@users.noreply.github.com>
2023-07-27 12:19:29 +0300

    Update Banking_Program.cpp
    Added comments

--- Banking_Program.cpp
+++ Banking_Program.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 using namespace std;
 
+// Function prototypes
 void showBalance(double balance);
 double deposit();
 double withdraw(double balance);
@@ -11,6 +12,8 @@ int main()
 {
     double balance = 0;
     int choice = 0;
+
+    // Main loop to display the menu and handle user choices
     do
     {
         cout << "Enter your choice: \n";
@@ -19,6 +22,7 @@ int main()
         cout << "3. Withdraw money\n";
         cout << "4. Exit\n";
 
+        // Input validation for choice
         if (!(cin >> choice))
         {
             cout << "Invalid input. Please enter a number.\n";
@@ -29,24 +33,25 @@ int main()
 
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear input buffer
 
+        // Handle user's choice using a switch statement
         switch (choice)
         {
         case 1:
-            showBalance(balance);
+            showBalance(balance); // Show current balance
             break;
         case 2:
-            balance += deposit();
-            showBalance(balance);
+            balance += deposit(); // Add deposited amount to the balance
+            showBalance(balance); // Show updated balance
             break;
         case 3:
-            balance -= withdraw(balance);
-            showBalance(balance);
+            balance -= withdraw(balance); // Subtract withdrawn amount from the balance
+            showBalance(balance); // Show updated balance
             break;
         case 4:
-            cout << "Thanks for visiting!\n";
+            cout << "Thanks for visiting!\n"; // Exit the program
             break;
         default:
-            cout << "Invalid choice\n";
+            cout << "Invalid choice\n"; // Displayed for an invalid menu option
         }
 
     } while (choice != 4);
@@ -54,11 +59,13 @@ int main()
     return 0;
 }
 
+// Function to display the current balance
 void showBalance(double balance)
 {
     cout << "Your balance is: $" << setprecision(2) << fixed << balance << "\n";
 }
 
+// Function to handle the deposit process
 double deposit()
 {
     double amount = 0;
@@ -67,15 +74,16 @@ double deposit()
 
     if (amount > 0)
     {
-        return amount;
+        return amount; // Return the valid deposit amount
     }
     else
     {
         cout << "That's not a valid amount: ";
-        return 0;
+        return 0; // Return 0 if the deposit amount is invalid
     }
 }
 
+// Function to handle the withdrawal process
 double withdraw(double balance)
 {
     double amount = 0;
@@ -85,15 +93,15 @@ double withdraw(double balance)
     if (amount < 0)
     {
         cout << "That's not a valid amount\n";
-        return 0;
+        return 0; // Return 0 if the withdrawal amount is negative
     }
     else if (amount > balance)
     {
         cout << "Insufficient funds\n";
-        return 0;
+        return 0; // Return 0 if the withdrawal amount exceeds the available balance
     }
     else
     {
-        return amount;
+        return amount; // Return the valid withdrawal amount
     }
 }
